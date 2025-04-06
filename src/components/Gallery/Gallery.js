11dfec81b4f7b038eb529295f2763dfa27e11145
Krzysztof <krzysztof.pawik@gmail.com>
2025-04-06 20:13:02 +0200

    Transfering gallery to its own component

---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './galleryStyle.css';
+
+function Gallery({ photos }) {
+    const galleryLayouts = ['50/50', '40/60', '60/40'];
+
+    return (
+        <div className='galleryMainFrame'>
+            {photos.reduce((rows, photo, index, array) => {
+                if (index % 2 === 0) {
+                    rows.push(array.slice(index, index + 2));
+                }
+                return rows;
+            }, []).map((pair, rowIndex) => {
+                const layout = galleryLayouts[rowIndex % galleryLayouts.length];
+                const [width1, width2] = layout.split('/').map(Number);
+                const photo1 = pair[0];
+                const photo2 = pair[1];
+
+                return (
+                    <div className='galleryRow' key={`row-${rowIndex}`}>
+                        {photo1 && (
+                            <div className='galleryBox' style={{ width: `${width1}%`, margin: '10px 10px 0 10px'}}>
+                                <img src={photo1.image} alt='' className='galleryImageStyle' key={photo1.id} />
+                            </div>
+                        )}
+                        {photo2 && (
+                            <div className='galleryBox' style={{ width: `${width2}%`, margin: '10px 10px 0 0' }}>
+                                <img src={photo2.image} alt='' className='galleryImageStyle' key={photo2.id} />
+                            </div>
+                        )}
+                        {!photo2 && width2 > 0 && (
+                            <div className='galleryBox' style={{ width: `100%`, height: '100%' }}>
+                                {/* Puste miejsce */}
+                            </div>
+                        )}
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
+
+export default Gallery;
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './homePageStyle.css';
+import Gallery from '../Gallery/Gallery';
 
 import MainVideo from "../../assets/MainVideo.mp4";
 
@@ -15,8 +16,6 @@ const photoDataContent = [
 ];
 
 function HomePage() {
-    const galleryLayouts = ['50/50', '40/60', '60/40'];
-
     return (
         <div className="mainBox">
             <div className='mainVideoContainer'>
@@ -29,39 +28,7 @@ function HomePage() {
                 />
             </div>
 
-            <div className='galleryMainFrame'>
-                {photoDataContent.reduce((rows, photo, index, array) => {
-                    if (index % 2 === 0) {
-                        rows.push(array.slice(index, index + 2));
-                    }
-                    return rows;
-                }, []).map((pair, rowIndex) => {
-                    const layout = galleryLayouts[rowIndex % galleryLayouts.length];
-                    const [width1, width2] = layout.split('/').map(Number);
-                    const photo1 = pair[0];
-                    const photo2 = pair[1];
-
-                    return (
-                        <div className='galleryRow' key={`row-${rowIndex}`}>
-                            {photo1 && (
-                                <div className='galleryBox' style={{ width: `${width1}%`, margin: '10px 10px 0 10px'}}>
-                                    <img src={photo1.image} alt='' className='galleryImageStyle' key={photo1.id} />
-                                </div>
-                            )}
-                            {photo2 && (
-                                <div className='galleryBox' style={{ width: `${width2}%`, margin: '10px 10px 0 0' }}>
-                                    <img src={photo2.image} alt='' className='galleryImageStyle' key={photo2.id} />
-                                </div>
-                            )}
-                            {!photo2 && width2 > 0 && (
-                                <div className='galleryBox' style={{ width: `100%`, height: '100%' }}>
-                                    {/* Puste miejsce */}
-                                </div>
-                            )}
-                        </div>
-                    );
-                })}
-            </div>
+            <Gallery photos={photoDataContent} />
         </div>
     );
 }
