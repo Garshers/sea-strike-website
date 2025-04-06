8c3f309f5bc2ee0ca808d0c84b609b1c1a13f903
Krzysztof <krzysztof.pawik@gmail.com>
2025-04-06 18:19:35 +0200

    Adding MainVideo to HomePage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './homePageStyle.css';
+
+import MainVideo from "../../assets/MainVideo.mp4";
 
 function HomePage() {
     return (
         <div className="mainBox">
+            <video src={MainVideo} width="100%" height="auto" autoplay="true" loop="true" muted="true" />
         </div>
     );
 }
